36e027fec11ef8a113a558778615aa2d181ea172
karen-servin <kads.servin@gmail.com>
2022-01-05 16:50:14 -0600

    some change in GCE

--- enviroments/dev/GCE/main.tf
+++ enviroments/dev/GCE/main.tf
@@ -1,8 +1,8 @@
 module "create_vpc" {
-  source            = "git@github.com:kads-servin/GCP-RD.git//modules/Network/"
-  project_id        = "kads-demo-337219"
-  vpc_name          = "vpc-name"
-  subnet_name       = "subnet-name"
+  source      = "git@github.com:kads-servin/GCP-RD.git//modules/Network/"
+  project_id  = "kads-demo-337219"
+  vpc_name    = "vpc-name"
+  subnet_name = "subnet-name"
 }
 
 module "create_gce" {
@@ -10,7 +10,9 @@ module "create_gce" {
   project_id   = "kads-demo-337219"
   name         = "gce-kads-demo"
   machine_type = "n2d-standard-2"
-  vpc_name     = "vpc-name"
-  subnet_name  = "subnet-name"
+  vpc_name     = module.create_vpc.vpc_selflink
+  subnet_name  = module.create_vpc.subnet_selflink
   depends_on   = [module.create_vpc]
 }
+
+
