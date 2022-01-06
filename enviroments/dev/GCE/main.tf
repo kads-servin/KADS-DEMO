a1800c4b584b1816d306625dc6bb8a3dc7d4ff6b
karen-servin <kads.servin@gmail.com>
2022-01-06 17:25:55 -0600

    changing source of gce

--- enviroments/dev/GCE/main.tf
+++ enviroments/dev/GCE/main.tf
@@ -1,12 +1,12 @@
 module "create_vpc" {
-  source      = "git@github.com:kads-servin/GCP-RD.git//modules/Network/"
+  source      = "github.com:kads-servin/GCP-RD.git//modules/Network/"
   project_id  = var.project_id
   vpc_name    = "vpc-name"
   subnet_name = "subnet-name"
 }
 
 module "create_gce" {
-  source       = "git@github.com:kads-servin/GCP-RD.git//modules/GCE/"
+  source       = "github.com:kads-servin/GCP-RD.git//modules/GCE/"
   project_id   = var.project_id
   name         = "gce-kads-demo"
   machine_type = "n2d-standard-2"
