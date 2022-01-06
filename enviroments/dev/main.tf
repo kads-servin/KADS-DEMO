23aa6278ee3ba13b9443f6cd600ee0e37fb6cb8e
karen-servin <kads.servin@gmail.com>
2022-01-06 17:49:48 -0600

    changing the main

--- enviroments/dev/main.tf
+++ enviroments/dev/main.tf
@@ -1,8 +1,18 @@
+module "create_vpc" {
+  source      = "git::https://github.com/kads-servin/GCP-RD.git//modules/Network/"
+  project_id  = var.project_id
+  vpc_name    = "vpc-name"
+  subnet_name = "subnet-name"
+}
 
 module "create_gce" {
-  source       = "./GCE"
+  source       = "git::https://github.com/kads-servin/GCP-RD.git//modules/GCE/"
   project_id   = var.project_id
-#   depends_on   = [module.create_vpc]
+  name         = "gce-kads-demo"
+  machine_type = "n2d-standard-2"
+  vpc_name     = module.create_vpc.vpc_selflink
+  subnet_name  = module.create_vpc.subnet_selflink
+  depends_on   = [module.create_vpc]
 }
 
 resource "google_pubsub_topic" "pubsub_topic_demo" {
